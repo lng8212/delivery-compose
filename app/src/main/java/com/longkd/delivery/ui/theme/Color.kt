c4b4640f3f6682ab1c0f1058eb9b38933e7a8eb5
Larry <long05012001@gmail.com>
2024-10-30 23:27:03 +0700

    Initial commit

---
+++ app/src/main/java/com/longkd/delivery/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.longkd.delivery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Onboarding = Color(0xFFA259FF)
+val BackgroundOnboarding = Color(0xFFF6F5F5)
+val TextPrimary = Color(0xFF2D0C57)
+val TextSecondary = Color(0xFF9586A8)
+val PrimaryButton = Color(0XFF0BCE83)
